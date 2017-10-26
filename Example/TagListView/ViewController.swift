f29684b7bccbb08ca3e43c1a9a1dee1b6b459c77
Jose Lino <josen.aceleracao@wizsolucoes.com.br>
2017-10-26 14:08:42 -0200

    Initial commit

---
+++ Example/TagListView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TagListView
+//
+//  Created by linox on 10/26/2017.
+//  Copyright (c) 2017 linox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
